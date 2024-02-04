147835e7e7b70d92b39b52a2af05e5acd7422e80
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2024-02-04 03:20:27 -0600

    Add files via upload

---
+++ hexremoval.py
@@ -0,0 +1,19 @@
+import os
+
+folder_input = input("Enter the path for the output folder: ")
+folder_output = folder_input+r'_Extracted'
+
+bytes_toremove = 56 # For Break the Chains should be 56 bytes
+
+
+os.makedirs(folder_output, exist_ok=True)
+
+def RemoveBytes(file):
+    with open(folder_input + "\\" + file, 'rb') as f:
+        data = f.read()
+    with open(folder_output + "\\" + file, 'wb') as f:
+        f.write(data[bytes_toremove:])
+
+
+for file in os.listdir(folder_input):
+    RemoveBytes(file)
